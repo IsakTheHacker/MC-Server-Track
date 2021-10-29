28d073d06d5d76d9b5771f0c7821d11568fc1161
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2021-10-29 17:47:15 +0200

    Set up environment

---
+++ MC_Server_Track.js
@@ -0,0 +1,30 @@
+const { Client, Intents, Collection } = require("discord.js");
+const dotenv = require('dotenv');
+
+const client = new Client(
+	{
+		partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+		intents: [
+			Intents.FLAGS.GUILDS,
+			Intents.FLAGS.GUILD_MEMBERS,
+			Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+			Intents.FLAGS.DIRECT_MESSAGES,
+			Intents.FLAGS.GUILD_MESSAGES,
+			Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+			Intents.FLAGS.GUILD_PRESENCES,
+		]
+	}
+);
+
+dotenv.config();
+const token = process.env.token;
+
+// functions.initWebserver(client);	//Start a web server
+
+client.commands = new Collection();
+
+// ["command_handler", "event_handler"].forEach(handler => {
+// 	require(`./handlers/${handler}.js`)(client);
+// });
+
+client.login(token);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MC-Server-Track
+# MC Server Track
 MC Server Track is a Discord bot capable of pinging and tracking the uptime of Minecraft servers!
