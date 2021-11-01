02ff71020af5fbaf12828c2e508ab750c71012e6
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2021-11-01 14:42:11 +0100

    Respond to button presses

--- commands/settings.js
+++ commands/settings.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Log } = require('@root/logSystem');
-const ms = require("ms");
 
 module.exports = {
 	name: "settings",
@@ -19,9 +18,9 @@ module.exports = {
 		const row = new Discord.MessageActionRow()
 			.addComponents(
 				new Discord.MessageButton()
-					.setCustomId("primary")
-					.setLabel("➡️")
-					.setStyle("PRIMARY"),
+					.setCustomId("continue")
+					.setEmoji("➡️")
+					.setStyle("SECONDARY")
 			);
 
 		const embed = new Discord.MessageEmbed()
@@ -29,6 +28,27 @@ module.exports = {
 			.setThumbnail(message.member.user.avatarURL())
 			.setTitle(`Change my user settings`)
 			.setDescription(`Hi, ${message.member.user.username}! I will help you change your user settings. Press :arrow_right: to continue!`);
-		Log.dcReply(message, { embeds: [embed], ephemeral: true, components: [row] });
+		await Log.dcReply(message, { embeds: [embed], ephemeral: true, components: [row], fetchReply: true });
+
+		let n = 0;
+		const filter = i => i.customId === "continue" && i.user.id === message.member.id;
+		const collector = message.channel.createMessageComponentCollector({ filter, componentType: "BUTTON" });
+
+		collector.on("collect", async i => {
+			switch (n) {
+			case 0:
+				const embed = new Discord.MessageEmbed()
+					.setColor("#1f1f1f")
+					.setTitle(`Change my user settings`)
+					.setDescription(`Page 1`);
+				await i.update({ embeds: [embed], components: [row] });
+				break;
+			}
+			n++;
+		});
+
+		collector.on("end", async (collected) => {
+			//Nothing
+		});
 	}
 }
