3fe5be5eafba23d02b5ac3f928be1e90526584d4
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2021-11-01 12:44:52 +0100

    Add info command

---
+++ commands/info.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Log } = require('@root/logSystem');
+const ms = require("ms");
+
+module.exports = {
+	name: "info",
+	aliases: [],
+	description: "View information about the bot.",
+	usage: [],
+	perms: [],
+	cooldown: 5,
+	enabled: true,
+	category: "miscellaneous",
+	data: new SlashCommandBuilder()
+		.setName("info")
+		.setDescription("View information about the bot."),
+	async do(message, args, userData) {
+		let currentTime = new Date().getTime();
+		let restartTime = new Date(currentTime - message.client.uptime);
+
+		const embed = new Discord.MessageEmbed()
+			.setColor("#1f1f1f")
+			.setThumbnail(message.client.user.avatarURL())
+			.setTitle(`${message.client.user.username}`)
+			.addFields(
+				{ name: "Statistics", value: `Uptime:  ‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎  ‎  ‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ **${ms(message.client.uptime)}** (${message.client.uptime} milliseconds)\nLast restart:  ‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  **${restartTime.toLocaleTimeString()}** (${restartTime.toLocaleDateString()})`, inline: true },
+				{ name: "Total servers", value: message.client.guilds.cache.size.toString(), inline: true }
+
+			);
+		Log.dcReply(message, { embeds: [embed] });
+	}
+}
