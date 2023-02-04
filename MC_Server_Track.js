a6388df0d1c606478869523ea28ef4e297b10f7d
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2023-02-04 15:43:27 +0100

    Migrate discord.js from v13 to v14

--- MC_Server_Track.js
+++ MC_Server_Track.js
@@ -1,18 +1,22 @@
-const { Client, Intents, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
 require('dotenv').config();
 require('module-alias/register');
 const { initWebserver } = require("./handyFunctions");
 
 const client = new Client({
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+	partials: [
+		Partials.Message,
+		Partials.Channel,
+		Partials.Reaction,
+	],
 	intents: [
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MEMBERS,
-		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-		Intents.FLAGS.DIRECT_MESSAGES,
-		Intents.FLAGS.GUILD_MESSAGES,
-		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-		Intents.FLAGS.GUILD_PRESENCES,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildPresences,
 	]
 });
 
